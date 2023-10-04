03713e35d97d51cabf7d1d2675e702af71f36b5b
Andrey Saderdinov <andreysaderdinov@gmail.com>
2023-10-04 20:05:51 +0300

    flake8 full update

--- lyceum/settings.py
+++ lyceum/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     "homepage.apps.HomepageConfig",
     "about.apps.AboutConfig",
     "catalog.apps.CatalogConfig",
-    "django.contrib.admin",
+    # "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -90,16 +90,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.\
+            UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation.\
+            MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.\
+            CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.\
+            NumericPasswordValidator",
     },
 ]
 
